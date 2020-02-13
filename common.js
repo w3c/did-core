b54287f80e5331261e2697de4ad005930eec0091
Manu Sporny <msporny@digitalbazaar.com>
2020-02-12 22:21:23 -0500

    Update common.js respec functions.

--- common.js
+++ common.js
@@ -1,5 +1,9 @@
 /* globals omitTerms, respecConfig, $, require */
-/* exported linkCrossReferences, restrictReferences, fixIncludes */
+/* DID WG common Javascript ReSpec functions */
+/*  ... stolen from Gregg Kellogg of the JSON-LD 1.1 Working Group */
+/*  ... who stole it from Manu Sporny of the JSON-LD 1.0 Working Group */
+/*  ... who stole it from Shane McCarron, that beautiful, beautiful man. */
+/* exported restrictReferences */
 
 var ccg = {
   // Add as the respecConfig localBiblio variable
@@ -150,150 +154,167 @@ var ccg = {
   }
 };
 
-
-
 // We should be able to remove terms that are not actually
 // referenced from the common definitions
 //
-// the termlist is in a block of class "termlist", so make sure that
-// has an ID and put that ID into the termLists array so we can
-// interrogate all of the included termlists later.
-var termNames = [] ;
-var termLists = [] ;
-var termsReferencedByTerms = [] ;
+// Add class "preserve" to a definition to ensure it is not removed.
+//
+// the termlist is in a block of class "termlist".
+const termNames = [] ;
+const termsReferencedByTerms = [] ;
 
 function restrictReferences(utils, content) {
-    "use strict";
-    var base = document.createElement("div");
-    base.innerHTML = content;
+  const base = document.createElement("div");
+  base.innerHTML = content;
 
-    // New new logic:
-    //
-    // 1. build a list of all term-internal references
-    // 2. When ready to process, for each reference INTO the terms,
-    // remove any terms they reference from the termNames array too.
-    $.each(base.querySelectorAll("dfn"), function(i, item) {
-        var $t = $(item) ;
-        var titles = $t.getDfnTitles();
-        var dropit = false;
-        // do we have an omitTerms
-        if (window.hasOwnProperty("omitTerms")) {
-            // search for a match
-            $.each(omitTerms, function(j, term) {
-                if (titles.indexOf(term) !== -1) {
-                    dropit = true;
-                }
-            });
-        }
-        // do we have an includeTerms
-        if (window.hasOwnProperty("includeTerms")) {
-            var found = false;
-            // search for a match
-            $.each(includeTerms, function(j, term) {
-                if (titles.indexOf(term) !== -1) {
-                    found = true;
-                }
-            });
-            if (!found) {
-                dropit = true;
-            }
-        }
-        if (dropit) {
-            $t.parent().next().remove();
-            $t.parent().remove();
-        } else {
-            var n = $t.makeID("dfn", titles[0]);
-            if (n) {
-                termNames[n] = $t.parent() ;
-            }
-        }
-    });
+  // New new logic:
+  //
+  // 1. build a list of all term-internal references
+  // 2. When ready to process, for each reference INTO the terms,
+  // remove any terms they reference from the termNames array too.
+  const noPreserve = base.querySelectorAll("dfn:not(.preserve)");
+  for (const item of noPreserve) {
+    const $t = $(item);
+    const titles = $t.getDfnTitles();
+    const n = $t.makeID("dfn", titles[0]);
+    if (n) {
+      termNames[n] = $t.parent();
+    }
+  }
 
-    var $container = $(".termlist",base) ;
-    var containerID = $container.makeID("", "terms") ;
-    termLists.push(containerID) ;
-
-    return (base.innerHTML);
+  const $container = $(".termlist", base) ;
+  const containerID = $container.makeID("", "terms") ;
+  return (base.innerHTML);
 }
-// add a handler to come in after all the definitions are resolved
-//
-// New logic: If the reference is within a 'dl' element of
-// class 'termlist', and if the target of that reference is
-// also within a 'dl' element of class 'termlist', then
-// consider it an internal reference and ignore it.
 
-require(["core/pubsubhub"], function(respecEvents) {
-    "use strict";
-    respecEvents.sub('end', function(message) {
-        if (message === 'core/link-to-dfn') {
-            // all definitions are linked; find any internal references
-            $(".termlist a.internalDFN").each(function() {
-                var $r = $(this);
-                var id = $r.attr('href');
-                var idref = id.replace(/^#/,"") ;
-                if (termNames[idref]) {
-                    // this is a reference to another term
-                    // what is the idref of THIS term?
-                    var $def = $r.closest('dd') ;
-                    if ($def.length) {
-                        var $p = $def.prev('dt').find('dfn') ;
-                        var tid = $p.attr('id') ;
-                        if (tid) {
-                            if (termsReferencedByTerms[tid]) {
-                                termsReferencedByTerms[tid].push(idref);
-                            } else {
-                                termsReferencedByTerms[tid] = [] ;
-                                termsReferencedByTerms[tid].push(idref);
-                            }
-                        }
-                    }
-                }
-            }) ;
+require(["core/pubsubhub"], (respecEvents) => {
+  "use strict";
 
-            // clearRefs is recursive.  Walk down the tree of
-            // references to ensure that all references are resolved.
-            var clearRefs = function(theTerm) {
-                if ( termsReferencedByTerms[theTerm] ) {
-                    $.each(termsReferencedByTerms[theTerm], function(i, item) {
-                        if (termNames[item]) {
-                            delete termNames[item];
-                            clearRefs(item);
-                        }
-                    });
-                }
-                // make sure this term doesn't get removed
-                if (termNames[theTerm]) {
-                    delete termNames[theTerm];
-                }
-            };
+  respecEvents.sub('end', (message) => {
+    // remove data-cite on where the citation is to ourselves.
+    const selfDfns = document.querySelectorAll("dfn[data-cite^='" + respecConfig.shortName.toUpperCase() + "#']");
+    for (const dfn of selfDfns) {
+      delete dfn.dataset.cite;
+    }
 
-            // now termsReferencedByTerms has ALL terms that
-            // reference other terms, and a list of the
-            // terms that they reference
-            $("a.internalDFN").each(function () {
-                var $item = $(this) ;
-                var t = $item.attr('href');
-                var r = t.replace(/^#/,"") ;
-                // if the item is outside the term list
-                if ( ! $item.closest('dl.termlist').length ) {
-                    clearRefs(r);
-                }
-            });
+    // Update data-cite references to ourselves.
+    const selfRefs = document.querySelectorAll("a[data-cite^='" + respecConfig.shortName.toUpperCase() + "#']");
+    for (const anchor of selfRefs) {
+      anchor.href= anchor.dataset.cite.replace(/^.*#/,"#");
+      delete anchor.dataset.cite;
+    }
+  });
 
-            // delete any terms that were not referenced.
-            Object.keys(termNames).forEach(function(term) {
-                var $p = $("#"+term) ;
-                if ($p) {
-                    var tList = $p.getDfnTitles();
-                    $p.parent().next().remove();
-                    $p.parent().remove() ;
-                    tList.forEach(function( item ) {
-                        if (respecConfig.definitionMap[item]) {
-                            delete respecConfig.definitionMap[item];
-                        }
-                    });
-                }
-            });
+  // add a handler to come in after all the definitions are resolved
+  //
+  // New logic: If the reference is within a 'dl' element of
+  // class 'termlist', and if the target of that reference is
+  // also within a 'dl' element of class 'termlist', then
+  // consider it an internal reference and ignore it.
+  respecEvents.sub('end', (message) => {
+    if (message === 'core/link-to-dfn') {
+      // all definitions are linked; find any internal references
+      const internalTerms = document.querySelectorAll(".termlist a.internalDFN");
+      for (const item of internalTerms) {
+        const idref = item.getAttribute('href').replace(/^#/,"") ;
+        if (termNames[idref]) {
+          // this is a reference to another term
+          // what is the idref of THIS term?
+          const def = item.closest('dd');
+          if (def) {
+            const tid = def.previousElementSibling
+              .querySelector('dfn')
+              .getAttribute('id');
+            if (tid) {
+              if (termsReferencedByTerms[tid] === undefined) termsReferencedByTerms[tid] = [];
+              termsReferencedByTerms[tid].push(idref);
+            }
+          }
         }
-    });
+      }
+
+      // clearRefs is recursive.  Walk down the tree of
+      // references to ensure that all references are resolved.
+      const clearRefs = (theTerm) => {
+        if (termsReferencedByTerms[theTerm] ) {
+          for (const item of termsReferencedByTerms[theTerm]) {
+            if (termNames[item]) {
+                delete termNames[item];
+                clearRefs(item);
+            }
+          }
+        };
+        // make sure this term doesn't get removed
+        if (termNames[theTerm]) {
+          delete termNames[theTerm];
+        }
+      };
+
+      // now termsReferencedByTerms has ALL terms that
+      // reference other terms, and a list of the
+      // terms that they reference
+      const internalRefs = document.querySelectorAll("a[data-link-type='dfn']");
+      for (const item of internalRefs) {
+        const idref = item.getAttribute('href').replace(/^.*#/,"") ;
+        // if the item is outside the term list
+        if (!item.closest('dl.termlist')) {
+          clearRefs(idref);
+        }
+      }
+
+      // delete any terms that were not referenced.
+      for (const term in termNames) {
+        const $p = $("#"+term);
+        // Remove term definitions inside a dt, where data-cite does not start with shortname
+        if ($p === undefined) { continue; }
+        if (!$p.parent().is("dt")) { continue; }
+        if (($p.data("cite") || "").toLowerCase().startsWith(respecConfig.shortName)) { continue; }
+
+        const $dt = $p.parent();
+        const $dd = $dt.next();
+
+        // If the associated dd contains a dfn which is _not_ in termNames, warn
+        if ($dd.children("dfn").length > 0) {
+          console.log(term + " definition contains definitions " + $dd.children("dfn").attr("id"))
+        }
+        console.log("drop term " + term);
+        const tList = $p.getDfnTitles();
+        $dd.remove(); // remove dd
+        $dt.remove(); // remove dt
+      }
+    }
+  });
 });
+
+function _esc(s) {
+  return s.replace(/&/g,'&amp;')
+    .replace(/>/g,'&gt;')
+    .replace(/"/g,'&quot;')
+    .replace(/</g,'&lt;');
+}
+
+function reindent(text) {
+  // TODO: use trimEnd when Edge supports it
+  const lines = text.trimRight().split("\n");
+  while (lines.length && !lines[0].trim()) {
+    lines.shift();
+  }
+  const indents = lines.filter(s => s.trim()).map(s => s.search(/[^\s]/));
+  const leastIndent = Math.min(...indents);
+  return lines.map(s => s.slice(leastIndent)).join("\n");
+}
+
+function updateExample(doc, content) {
+  // perform transformations to make it render and prettier
+  return _esc(reindent(unComment(doc, content)));
+}
+
+function unComment(doc, content) {
+  // perform transformations to make it render and prettier
+  return content
+    .replace(/<!--/, '')
+    .replace(/-->/, '')
+    .replace(/< !\s*-\s*-/g, '<!--')
+    .replace(/-\s*- >/g, '-->')
+    .replace(/-\s*-\s*&gt;/g, '--&gt;');
+}
